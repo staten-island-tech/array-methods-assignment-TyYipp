9d4e499d9035b9b0ded7f2d803e03ea93a3ab5fd
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-10-06 10:15:04 -0400

    10/6
    doing stuff for array project

--- object.js
+++ object.js
@@ -1,23 +1,40 @@
-const students = [{
-    name: "ricky",
-    grade: 10,
-    intellegent: false,
-    braincells: null,
-    favorite_food: ["fried chicken", "mac and cheeze", "corn bread", "doritos"]
-},
+const date = new Date();
+let year = date.getFullYear
+        const students = [
+            {
+            name: "ricky",
+            grade: 10,
+            intellegent: false,
+            braincells: null,
+            favorite_food: ["fried chicken", "mac and cheeze", "corn bread", "doritos"],
+            DOB: 2011,
+            age: function(){
+                return year - this.DOB;
+        },
+    },
 {
-    name: "harry",
-    grade: 10,
-    intellegent: false,
-    braincells: true,
-    favorite_food: ["ramen", "sushi", "uni", "wagyu"]
-},
+            name: "harry",
+            grade: 10,
+            intellegent: false,
+            braincells: true,
+            favorite_food: ["ramen", "sushi", "uni", "wagyu"],
+            DOB: 2011,
+            age: function(){
+                return year - this.DOB;
+        },
+    },
 {
-    name: "ty",
-    grade: 10,
-    intellegent: true,
-    braincells: true,
-    favorite_food: ["mashed potatoes", "brocolii", "ice cream", "jello"]
-}
+            name: "ty",
+            grade: 10,
+            intellegent: true,
+            braincells: true,
+            favorite_food: ["mashed potatoes", "brocolii", "curry", "fried rice"],
+            DOB: 2008,
+            age: function(){
+                return year - this.DOB;
+         }
+    }
 ]
-students.name.forEach((element) => console.log(element));
+// students.forEach((student) => console.log(student.name))
+const braincells = students.filter((student) => console.log(student.braincells))
+console.log(braincells);
